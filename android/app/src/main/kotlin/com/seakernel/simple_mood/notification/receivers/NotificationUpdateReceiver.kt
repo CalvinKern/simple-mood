658e9d7dca6a98ec87f39c8b54a05ad711227806
CalvinKern <calvinmkern@gmail.com>
2021-11-27 20:09:14 -0700

    Fix: new updates no longer clear out notifications in alarm manager

---
+++ android/app/src/main/kotlin/com/seakernel/simple_mood/notification/receivers/NotificationUpdateReceiver.kt
@@ -0,0 +1,13 @@
+package com.seakernel.simple_mood.notification.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.seakernel.simple_mood.notification.NotificationPlugin
+
+class NotificationUpdateReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null || intent?.action != Intent.ACTION_MY_PACKAGE_REPLACED) return
+        NotificationPlugin.resetAlarms(context)
+    }
+}
